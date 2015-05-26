d2e2cb36f91a541add533144600089c1777ee79f
Bryan Robbins <bryantrobbins@gmail.com>
2015-05-25 20:46:28 -0400

    Removing hack build script

--- latest.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-echo Building image
-id=$(docker build . | grep "Successfully built" | awk '{print $3}')
-echo Image id is $id
-echo Tagging image
-docker tag -f $id bryantrobbins/www:latest
-echo Pushing image to Docker Hub
-docker push bryantrobbins/www
-echo Done
